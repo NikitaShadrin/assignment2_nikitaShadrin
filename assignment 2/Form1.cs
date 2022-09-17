7cfc03d92402c4671faf94baed560e6d26ad732b
ADMIN <ADMIN@DESKTOP-KMHGASB>
2022-09-17 12:24:01 +0300

    Add project files.

---
+++ assignment 2/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace assignment_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+            textBox6.Text = "";
+            textBox7.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+            int textA = Int32.Parse(textBox1.Text) * 15;
+            int textB = Int32.Parse(textBox2.Text) * 12;
+            int textC = Int32.Parse(textBox3.Text) * 9;
+            int resultText = textA + textB + textC;
+            textBox4.Text = textA.ToString();
+            textBox5.Text = textB.ToString();
+            textBox6.Text = textC.ToString();
+            textBox7.Text = resultText.ToString();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Please only input numbers into each text box");
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
